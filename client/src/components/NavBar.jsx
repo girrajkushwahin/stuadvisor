507d9df775e73729495816f0459e3de24795b87f
rootkaalsec <tgirrajindia@gmail.com>
2023-02-02 23:31:34 +0530

    formatting done

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -6,23 +6,19 @@ const NavBar = () => {
     return (
         <>
             <div className="nav-container nav-bar">
-                    <img src={logo} alt="" className='logo'/>
-                    <div className="mid-link">
-                        <ul className="nav-items">
-                            {/* <li className="li-item nav-item1 active">Home</li>
-                            <li className="li-item nav-item2">Blogs</li>
-                            <li className="li-item nav-item3">Accadmics</li>
-                            <li className="li-item nav-item4">Search Colleges</li> */}
-                            <NavLink to='/' className='li-item'>Home</NavLink>
-                            <NavLink to='/searchcolleges' className='li-item'>Search Colleges</NavLink>
-                            <NavLink to='/academics' className='li-item'>Academics</NavLink>
-                            <NavLink to='/blogs' className='li-item'>Blogs</NavLink>
-                        </ul>
-                    </div>
-                    <div className="search-box">
-                        <input type="text" placeholder='Search ' />
-                        <button className='search-btn'><i className="fa-solid fa-magnifying-glass"></i></button>
-                    </div>
+                <img src={logo} alt="" className='logo' />
+                <div className="mid-link">
+                    <ul className="nav-items">
+                        <NavLink to='/' className='li-item'>Home</NavLink>
+                        <NavLink to='searchcolleges' className='li-item'>Search Colleges</NavLink>
+                        <NavLink to='academics' className='li-item'>Academics</NavLink>
+                        <NavLink to='blogs' className='li-item'>Blogs</NavLink>
+                    </ul>
+                </div>
+                <div className="search-box">
+                    <input type="text" placeholder='Search ' />
+                    <button className='search-btn'><i className="fa-solid fa-magnifying-glass"></i></button>
+                </div>
             </div>
         </>
     )
