10a2223566e2c62f7a361e42dd710c8de370bd48
gopalkushwah <gopalkushwah15101999@gmail.com>
2023-02-04 00:03:59 +0530

    Edited footer , 404 page and css

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,11 +1,17 @@
-import React from 'react'
-
+import React from 'react';
+import { Link } from 'react-router-dom';
+import bottomImage from '../images/BottomLogo.png';
 const Footer = () => {
   const year = new Date().getFullYear();
   return (
     <>
       <div className="footer-container">
-        <p>©️ {year} StuAdvisor. All rights reserved | Terms & Conditions</p>
+        <div className="bottom-side">
+            <button className='sign-in sign-btn'>sign in</button>
+            <button className='sign-up sign-btn'>sign up</button>
+        </div>
+        <p className='copyright-pera'>©️ {year} StuAdvisor. All rights reserved | Terms & Conditions</p>
+        <Link to='/'><img src={bottomImage} alt="stuadvisor" className='footer-img'/></Link>
       </div>
     </>
   )
--- client/src/components/SideBar.jsx
+++ client/src/components/SideBar.jsx
@@ -3,16 +3,11 @@ import React from 'react';
 const SideBar = ({ setData }) => {
     return (
         <>
-
             <div className="main-item main-left">
                 <ul className='side-ul'>
                     {setData.map((elem,ind)=><li className='side-li' key={ind}>{elem.icon}<span>{elem.text}</span></li>
                     )}
                 </ul>
-                <div className="bottom-side">
-                    <button className='sign-in'>sign in</button>
-                    <button className='sign-up'>sign up</button>
-                </div>
             </div>
         </>
     )
