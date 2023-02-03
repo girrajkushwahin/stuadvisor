aedbf36ff36334e2fcc25b3218968af83bfca85d
gopalkushwah <gopalkushwah15101999@gmail.com>
2023-02-03 20:09:53 +0530

    Error page css edited

--- client/src/components/Error.jsx
+++ client/src/components/Error.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
-
+import errorImage from '../images/ErrorImage.jpg';
 const Error = () => {
   return (
     <>
-      <h1>404: not found</h1>
+    <div className="error-main-container">
+      <div className="error-page">
+        <div className="left-error">
+          <img src={errorImage} alt="" className="error-image" />
+        </div>
+        <div className="right-error">
+          <h1 className='error-heading'>Oops!<span className='error-span'>page not found on server</span></h1>
+          <p className='error-pera'>The link you followed is either outdeted,inaccurate,or the server has been instructed not to let you have it</p>
+          <button className='error-btn'>Go Back</button>
+        </div>
+      </div>
+    </div>
+      
     </>
   )
 }
