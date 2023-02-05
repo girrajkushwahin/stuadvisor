b6d2df542d7ecc38245b4ab6436f3e6f095460c0
rootkaalsec <tgirrajindia@gmail.com>
2023-02-05 22:19:11 +0530

    edited icons of sidebar in home component

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -4,9 +4,9 @@ import SignIn from './SignIn';
 import SignUp from './SignUp';
 import GetStarted from './GetStarted';
 import News from './News';
-import About from './About';
-import Contact from './Contact';
-import Last from './Last';
+import AboutUs from './AboutUs';
+import ContactUs from './ContactUs';
+import Reviews from './Reviews';
 
 const Home = () => {
   const [state, setState] = useState(0);
@@ -16,7 +16,7 @@ const Home = () => {
     setState(id);
   }
 
-  const homeMenu = [{ text: 'Get Started', icon: <i class="i-tag fa-solid fa-person-walking"></i>, click: menuClick }, { text: 'last', icon: <i className="i-tag fa-solid fa-magnifying-glass-chart"></i>, click: menuClick }, { text: 'News', icon: <i class="i-tag fa-solid fa-newspaper"></i>, click: menuClick }, { text: 'about', icon: <i class="i-tag fa-solid fa-address-card"></i>, click: menuClick }, { text: 'contact', icon: <i class="i-tag fa-solid fa-address-book"></i>, click: menuClick }, { text: 'Sign in', icon: <i className="i-tag fa-solid fa-magnifying-glass-chart"></i>, click: menuClick }, { text: 'Sign up', icon: <i className="i-tag fa-solid fa-magnifying-glass-chart"></i>, click: menuClick }];
+  const homeMenu = [{ text: 'Get Started', icon: <i class="i-tag fa-solid fa-person-walking"></i>, click: menuClick }, { text: 'Reviews', icon: <i class="i-tag fa-regular fa-comments"></i>, click: menuClick }, { text: 'News', icon: <i class="i-tag fa-solid fa-newspaper"></i>, click: menuClick }, { text: 'About Us', icon: <i class="i-tag fa-solid fa-address-card"></i>, click: menuClick }, { text: 'Contact Us', icon: <i class="i-tag fa-solid fa-address-book"></i>, click: menuClick }, { text: 'Sign in', icon: <i class="i-tag fa-solid fa-door-open"></i>, click: menuClick }, { text: 'Sign up', icon: <i class="i-tag fa-solid fa-user-plus"></i>, click: menuClick }];
 
   useEffect(() => {
     key(homeMenu);
@@ -27,10 +27,10 @@ const Home = () => {
     <>
       <div className="main-item main-right">
         {state === 0 ? <GetStarted /> : null}
-        {state === 1 ? <Last /> : null}
+        {state === 1 ? <Reviews /> : null}
         {state === 2 ? <News /> : null}
-        {state === 3 ? <About /> : null}
-        {state === 4 ? <Contact /> : null}
+        {state === 3 ? <AboutUs /> : null}
+        {state === 4 ? <ContactUs /> : null}
         {state === 5 ? <SignIn /> : null}
         {state === 6 ? <SignUp /> : null}
       </div>
---
+++ client/src/components/Reviews.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Reviews = () => {
+  return (
+    <>
+        <h1>Reviews page</h1>
+    </>
+  )
+}
+
+export default Reviews;
