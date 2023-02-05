b7c0a0505788b4d9e4924090f4ec4ca1a74cba58
gopalkushwah <gopalkushwah15101999@gmail.com>
2023-02-06 00:17:04 +0530

    sign in page done

--- client/src/components/SignIn.jsx
+++ client/src/components/SignIn.jsx
@@ -1,9 +1,27 @@
 import React from 'react';
-
+import ThreeDImage1 from '../images/3dImage2.jpg';
 const SignIn = () => {
   return (
     <>
-        <h1>SignIn page</h1>
+        <div className="sign-in-container">
+          <div className="left-sign-in">
+            <img className='left-sign-img' src={ThreeDImage1} alt="" />
+          </div>
+          <div className="right-sign-in">
+            <h1 className='right-heading'>Hello! <span className='right-span'>Good Morning</span></h1>
+            <form action="" className="sign-in-form">
+              <h1 className='heding-inside'>Login your Account</h1>
+              <label className='sign-in-tags' htmlFor="">Username</label>
+              <input className='sign-in-tags input-sign' placeholder='Username' type="text" required autoFocus/>
+              <label className='sign-in-tags' htmlFor="">Password</label>
+              <input className='sign-in-tags input-sign' placeholder='Password' type="password" required autoComplete='off'/>
+              <div className="btn-div">
+                <button className="sign-in-btn">Login</button>
+
+              </div>
+            </form>
+          </div>
+        </div>
     </>
   )
 }
--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
-import './index.css';
+import './layout.css';
+import './pages.css';
+
 import App from './App';
   
 const root = ReactDOM.createRoot(document.getElementById('root'));
