2ed1d34d10e2e8bfdb5a63ecf8898b2afb04a797
gopalkushwah <gopalkushwah15101999@gmail.com>
2023-02-05 20:15:26 +0530

    home complete

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -16,7 +16,7 @@ const Home = () => {
     setState(id);
   }
 
-  const homeMenu = [{ text: 'Get Started', icon: <i className="i-tag fa-solid fa-house"></i>, click: menuClick }, { text: 'News', icon: <i className="i-tag fa-solid fa-address-card"></i>, click: menuClick }, { text: 'about', icon: <i className="i-tag fa-sharp fa-solid fa-laptop"></i>, click: menuClick }, { text: 'contact', icon: <i className="i-tag fa-brands fa-blogger-b"></i>, click: menuClick }, { text: 'last', icon: <i className="i-tag fa-solid fa-magnifying-glass-chart"></i>, click: menuClick },{ text: 'Sign in', icon: <i className="i-tag fa-solid fa-magnifying-glass-chart"></i>, click: menuClick },{ text: 'Sign up', icon: <i className="i-tag fa-solid fa-magnifying-glass-chart"></i>, click: menuClick }];
+  const homeMenu = [{ text: 'Get Started', icon: <i class="i-tag fa-solid fa-person-walking"></i>, click: menuClick }, { text: 'last', icon: <i className="i-tag fa-solid fa-magnifying-glass-chart"></i>, click: menuClick }, { text: 'News', icon: <i class="i-tag fa-solid fa-newspaper"></i>, click: menuClick }, { text: 'about', icon: <i class="i-tag fa-solid fa-address-card"></i>, click: menuClick }, { text: 'contact', icon: <i class="i-tag fa-solid fa-address-book"></i>, click: menuClick }, { text: 'Sign in', icon: <i className="i-tag fa-solid fa-magnifying-glass-chart"></i>, click: menuClick }, { text: 'Sign up', icon: <i className="i-tag fa-solid fa-magnifying-glass-chart"></i>, click: menuClick }];
 
   useEffect(() => {
     key(homeMenu);
@@ -27,10 +27,10 @@ const Home = () => {
     <>
       <div className="main-item main-right">
         {state === 0 ? <GetStarted /> : null}
-        {state === 1 ? <News /> : null}
-        {state === 2 ? <About /> : null}
-        {state === 3 ? <Contact /> : null}
-        {state === 4 ? <Last /> : null}
+        {state === 1 ? <Last /> : null}
+        {state === 2 ? <News /> : null}
+        {state === 3 ? <About /> : null}
+        {state === 4 ? <Contact /> : null}
         {state === 5 ? <SignIn /> : null}
         {state === 6 ? <SignUp /> : null}
       </div>
