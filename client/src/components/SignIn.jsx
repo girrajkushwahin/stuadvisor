81c82df9f9c1c7a8487edafcd062b5fbc4d03c72
rootkaalsec <tgirrajindia@gmail.com>
2023-02-10 21:39:01 +0530

    edited sign in little bit

--- client/src/components/SignIn.jsx
+++ client/src/components/SignIn.jsx
@@ -3,27 +3,26 @@ import ThreeDImage1 from '../images/3dImage2.jpg';
 const SignIn = () => {
   return (
     <>
-     <div className="cont">
-          <div className="sign-in-container">
-            <div className="left-sign-in">
-              <img className='left-sign-img' src={ThreeDImage1} alt="" />
-            </div>
-            <div className="right-sign-in">
-              <h1 className='right-heading'>Hello! <span className='right-span'>Good Morning</span></h1>
-              <h1 className='heding-inside heading'>Login your Account</h1>
-              <form action="" className="sign-in-form">
-                <label className='sign-in-tags lables' htmlFor="">Username</label>
-                <input className='sign-in-tags input-sign' placeholder='Username' type="text" required autoFocus/>
-                <label className='sign-in-tags lables' htmlFor="">Password</label>
-                <input className='sign-in-tags input-sign' placeholder='Password' type="password" required autoComplete='off'/>
-                <div className="btn-div">
-                  <button className="sign-in-btn">Login</button>
-
-                </div>
-              </form>
-            </div>
+      <div className="cont">
+        <div className="sign-in-container">
+          <div className="left-sign-in">
+            <img className='left-sign-img' src={ThreeDImage1} alt="img" />
+          </div>
+          <div className="right-sign-in">
+            <h1 className='right-heading'>Hello! <span className='right-span'>Good Morning</span></h1>
+            <h1 className='heding-inside heading'>Login to your Account</h1>
+            <form action="" className="sign-in-form">
+              <label className='sign-in-tags lables' htmlFor="">Username</label>
+              <input className='sign-in-tags input-sign' placeholder='Username' type="text" required autoFocus />
+              <label className='sign-in-tags lables' htmlFor="">Password</label>
+              <input className='sign-in-tags input-sign' placeholder='Password' type="password" required autoComplete='off' />
+              <div className="btn-div">
+                <button className="sign-in-btn">Login</button>
+              </div>
+            </form>
           </div>
         </div>
+      </div>
     </>
   )
 }
