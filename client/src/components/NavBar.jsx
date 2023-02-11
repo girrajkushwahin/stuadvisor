59d20ca8a7514ecf5f9c43d0febea16729c51900
rootkaalsec <tgirrajindia@gmail.com>
2023-02-11 22:31:18 +0530

    completed navtoggle

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,48 +1,28 @@
-import React from 'react';
-import { Link, NavLink } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import NavToggle from './NavToggle';
 import logo from '../images/logo.png';
 
-const getToggle = ()=>{
-    // /*============ MENU SHOW ===========*/
-   const toggle = document.getElementById("nav-toggle"),
-   nav = document.getElementById("nav-menu");
-
-   if(toggle && nav){
-       toggle.addEventListener('click',()=>{
-           nav.classList.toggle('show');
-       })
-   }
-  // /*============ REMOVE MOBILE MENU ===========*/
-   const navLink = document.querySelectorAll('.li-item');
-   function linkAction(){
-       const navMenu = document.getElementById('nav-menu');
-       navMenu.classList.remove('show');
-   }
-   navLink.forEach(n => n.addEventListener('click',linkAction));
-  }
-
 const NavBar = () => {
-   
-// =======================================================================================
-    const navitems = [{ key: 'Home', path: '/' }, { key: 'Search College', path: 'searchcolleges' }, { key: 'Academics', path: 'academics' }, { key: 'Blogs', path: 'blogs' }];
+    const [toggle, setToggle] = useState(false);
+
+    const navToggle = () => {
+        if (toggle === false) setToggle(true);
+        else setToggle(false);
+    }
 
     return (
         <>
             <div className="nav-container nav-bar">
                 <Link to='/'><img src={logo} alt="stuadvisor" className='logo' /></Link>
-                <div className="mid-link" id='nav-menu'>
-                    {/* Add class - show */}
-                    <ul className="nav-items">
-                        {navitems.map((elem, ind) => <li className='nav-item' key={ind}><NavLink to={elem.path} className='li-item'>{elem.key}</NavLink></li>)}
-                    </ul>
-                </div>
+                {toggle===false?<NavToggle toggle={navToggle} data='mid-link' />:''}
+                {toggle===true?<NavToggle toggle={navToggle} data='mid-link show' />:''}
                 <div className="left-nav">
                     <div className="search-box">
                         <input type="text" placeholder='Search ' />
                         <button className='search-btn'><i className="fa-solid fa-magnifying-glass"></i></button>
                     </div>
-                    <div className="humberger-icon-nav"  ><i className="fa-solid fa-bars" id="nav-toggle" onClick={getToggle}></i></div>  
-                    {/* click here */}
+                    <div className="humberger-icon-nav"  ><i className="fa-solid fa-bars" id="nav-toggle" onClick={navToggle}></i></div>
                 </div>
             </div>
         </>
