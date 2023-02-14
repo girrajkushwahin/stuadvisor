4e5e929fc91578fd950420947a59f2c80653aeb7
gopalkushwah <gopalkushwah15101999@gmail.com>
2023-02-14 23:14:28 +0530

    started-contect-page

--- client/src/components/GetStarted.jsx
+++ client/src/components/GetStarted.jsx
@@ -12,7 +12,7 @@ const GetStarted = () => {
         <div className="btn-div">
           <button className='get-started-btn'>Check</button>
         </div>
-      </div>
+      
       <div className="get-started-container">
         <img src={getStarted2} alt="Search for your dream college" className='get-started-image'/>
         <h1 className='get-started-heading'>Search for your dream college</h1>
@@ -35,6 +35,7 @@ const GetStarted = () => {
         <p className='get-started-pera'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit culpa provident tenetur odit aliquam ratione quae commodi quas unde? Autem quod expedita laudantium eum nihil temporibus ipsam, neque itaque at id ex maxime iusto, quasi optio voluptas fuga ullam dolores quis minus libero accusamus dicta dignissimos a? Veniam voluptas doloremque expedita !</p>
         <div className="btn-div">
         </div>
+      </div>
       </div>
     </>
   )
