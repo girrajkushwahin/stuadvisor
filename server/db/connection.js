f591d6e961fba5330f45487eb4a519352d46bc01
rootkaalsec <tgirrajindia@gmail.com>
2023-02-17 21:40:40 +0530

    pushed

--- server/db/connection.js
+++ server/db/connection.js
@@ -4,5 +4,5 @@ const DB = 'mongodb://127.0.0.1:27017/stuadvisor';
 
 mongoose.set('strictQuery', false);
 mongoose.connect(DB).then(() => {
-    console.log('connected to DB');
+    console.log('connected to DB at port 27017');
 }).catch(err => console.log('error connecting db'));
