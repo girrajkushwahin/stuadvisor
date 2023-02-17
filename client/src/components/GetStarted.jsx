fb49194eb18a2dfa6cd52c4ce56454cdf7c0281e
rootkaalsec <tgirrajindia@gmail.com>
2023-02-18 00:23:50 +0530

    completed getstarted of home

--- client/src/components/GetStarted.jsx
+++ client/src/components/GetStarted.jsx
@@ -1,42 +1,17 @@
 import React from 'react';
-import getStarted1 from '../images/getStarted1.jpg';
-import getStarted2 from '../images/getStarted2.jpg';
-import getStarted3 from '../images/getStarted3.jpg';
+import getStartedData from './Data';
+
 const GetStarted = () => {
   return (
     <>
-      <div className="get-started-container">
-        <img src={getStarted1} alt="Search for your dream college" className='get-started-image'/>
-        <h1 className='get-started-heading'>Search for your dream college</h1>
-        <p className='get-started-pera'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit culpa provident tenetur odit aliquam ratione quae commodi quas unde? Autem quod expedita laudantium eum nihil temporibus ipsam, neque itaque at id ex maxime iusto, quasi optio voluptas fuga ullam dolores quis minus libero accusamus dicta dignissimos a? Veniam voluptas doloremque expedita !</p>
-        <div className="btn-div">
-          <button className='get-started-btn'>Check</button>
+      {getStartedData.map((val, ind) =>
+        <div className="get-started-container" key={ind}>
+          <img src={val.img} alt="home-img" className='get-started-image' />
+          <h1 className='get-started-heading'>{val.heading}</h1>
+          <p className='get-started-pera'>{val.content}</p>
+          <div className="btn-div">{val.button}</div>
         </div>
-      </div>
-      <div className="get-started-container">
-        <img src={getStarted2} alt="Search for your dream college" className='get-started-image'/>
-        <h1 className='get-started-heading'>Search for your dream college</h1>
-        <p className='get-started-pera'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit culpa provident tenetur odit aliquam ratione quae commodi quas unde? Autem quod expedita laudantium eum nihil temporibus ipsam, neque itaque at id ex maxime iusto, quasi optio voluptas fuga ullam dolores quis minus libero accusamus dicta dignissimos a? Veniam voluptas doloremque expedita !</p>
-        <div className="btn-div">
-          <button className='get-started-btn'>Check</button>
-        </div>
-      </div>
-      <div className="get-started-container">
-        <img src={getStarted3} alt="Search for your dream college" className='get-started-image'/>
-        <h1 className='get-started-heading'>Search for your dream college</h1>
-        <p className='get-started-pera'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit culpa provident tenetur odit aliquam ratione quae commodi quas unde? Autem quod expedita laudantium eum nihil temporibus ipsam, neque itaque at id ex maxime iusto, quasi optio voluptas fuga ullam dolores quis minus libero accusamus dicta dignissimos a? Veniam voluptas doloremque expedita !</p>
-        <div className="btn-div">
-          <button className='get-started-btn'>Check</button>
-        </div>
-      </div>
-      <div className="get-started-container">
-        <img src={getStarted1} alt="Search for your dream college" className='get-started-image'/>
-        <h1 className='get-started-heading'>Search for your dream college</h1>
-        <p className='get-started-pera'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit culpa provident tenetur odit aliquam ratione quae commodi quas unde? Autem quod expedita laudantium eum nihil temporibus ipsam, neque itaque at id ex maxime iusto, quasi optio voluptas fuga ullam dolores quis minus libero accusamus dicta dignissimos a? Veniam voluptas doloremque expedita !</p>
-        <div className="btn-div">
-        </div>
-      </div>
-      
+      )}
     </>
   )
 }
