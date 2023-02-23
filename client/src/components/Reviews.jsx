0b271e5372de05269e731dfcfec2ea10ea59a330
gopalkushwah <gopalkushwah15101999@gmail.com>
2023-02-23 19:13:38 +0530

    Review page completed

--- client/src/components/Reviews.jsx
+++ client/src/components/Reviews.jsx
@@ -3,7 +3,180 @@ import React from 'react';
 const Reviews = () => {
   return (
     <>
-        <h1>Reviews page</h1>
+      <div className="review-main-container">
+        <div className="heading-review-container">
+          <h1>What students say about us</h1>
+          <p>Your <span>Reviews</span> are important to develope our community more strong . reviews help people to know more and learn more</p>
+        </div>
+        <hr />
+        <div className="top-reviews">
+          <h1><span>T</span>op <span>R</span>views</h1>
+        </div>
+        <div className="reviews-container">
+          <div className="reviews">
+            <p>Review by</p>
+            <h1>Gopal kushwah</h1>
+            <p>⭐⭐⭐⭐⭐</p>
+            <hr />
+            <div className='review-content'><p>luptatibus, reiciendis. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quia, suscipit?</p></div>
+          </div>
+          <div className="reviews">
+            <p>Review by</p>
+            <h1>Gopal kushwah</h1>
+            <p>⭐⭐⭐⭐⭐</p>
+            <hr />
+            <div className='review-content'><p>luptatibus, reiciendis. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quia, suscipit?</p></div>
+
+          </div>
+          <div className="reviews">
+            <p>Review by</p>
+            <h1>Gopal kushwah</h1>
+            <p>⭐⭐⭐⭐⭐</p>
+            <hr />
+            <div className='review-content'><p>luptatibus, reiciendis. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quia, suscipit?</p></div>
+
+          </div>
+          <div className="reviews">
+            <p>Review by</p>
+            <h1>Gopal kushwah</h1>
+            <p>⭐⭐⭐⭐⭐</p>
+            <hr />
+            <div className='review-content'><p>luptatibus, reiciendis. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quia, suscipit?</p></div>
+
+          </div>
+          <div className="reviews">
+            <p>Review by</p>
+            <h1>Gopal kushwah</h1>
+            <p>⭐⭐⭐⭐⭐</p>
+            <hr />
+            <div className='review-content'><p>luptatibus, reiciendis. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quia, suscipit?</p></div>
+          </div>
+          <div className="reviews">
+            <p>Review by</p>
+            <h1>Gopal kushwah</h1>
+            <p>⭐⭐⭐⭐⭐</p>
+            <hr />
+            <div className='review-content'><p>luptatibus, reiciendis. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quia, suscipit?</p></div>
+
+          </div>
+          <div className="reviews">
+            <p>Review by</p>
+            <h1>Gopal kushwah</h1>
+            <p>⭐⭐⭐⭐⭐</p>
+            <hr />
+            <div className='review-content'><p>luptatibus, reiciendis. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quia, suscipit?</p></div>
+
+          </div>
+          <div className="reviews">
+            <p>Review by</p>
+            <h1>Gopal kushwah</h1>
+            <p>⭐⭐⭐⭐⭐</p>
+            <hr />
+            <div className='review-content'><p>luptatibus, reiciendis. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quia, suscipit?</p></div>
+
+          </div>
+        </div>
+        <hr />
+        <div className="top-reviews">
+          <h1><span>M</span>ore <span>R</span>views</h1>
+        </div>
+        <div className="reviews-container">
+          <div className="reviews">
+            <p>Review by</p>
+            <h1>Gopal kushwah</h1>
+            <p>⭐⭐⭐⭐⭐</p>
+            <hr />
+            <div className='review-content'><p>luptatibus, reiciendis. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quia, suscipit?</p></div>
+          </div>
+          <div className="reviews">
+            <p>Review by</p>
+            <h1>Gopal kushwah</h1>
+            <p>⭐⭐⭐⭐⭐</p>
+            <hr />
+            <div className='review-content'><p>luptatibus, reiciendis. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quia, suscipit?</p></div>
+
+          </div>
+          <div className="reviews">
+            <p>Review by</p>
+            <h1>Gopal kushwah</h1>
+            <p>⭐⭐⭐⭐⭐</p>
+            <hr />
+            <div className='review-content'><p>luptatibus, reiciendis. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quia, suscipit?</p></div>
+
+          </div>
+          <div className="reviews">
+            <p>Review by</p>
+            <h1>Gopal kushwah</h1>
+            <p>⭐⭐⭐⭐⭐</p>
+            <hr />
+            <div className='review-content'><p>luptatibus, reiciendis. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quia, suscipit?</p></div>
+
+          </div>
+          <div className="reviews">
+            <p>Review by</p>
+            <h1>Gopal kushwah</h1>
+            <p>⭐⭐⭐⭐⭐</p>
+            <hr />
+            <div className='review-content'><p>luptatibus, reiciendis. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quia, suscipit?</p></div>
+          </div>
+          <div className="reviews">
+            <p>Review by</p>
+            <h1>Gopal kushwah</h1>
+            <p>⭐⭐⭐⭐⭐</p>
+            <hr />
+            <div className='review-content'><p>luptatibus, reiciendis. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quia, suscipit?</p></div>
+
+          </div>
+          <div className="reviews">
+            <p>Review by</p>
+            <h1>Gopal kushwah</h1>
+            <p>⭐⭐⭐⭐⭐</p>
+            <hr />
+            <div className='review-content'><p>luptatibus, reiciendis. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quia, suscipit?</p></div>
+
+          </div>
+          <div className="reviews">
+            <p>Review by</p>
+            <h1>Gopal kushwah</h1>
+            <p>⭐⭐⭐⭐⭐</p>
+            <hr />
+            <div className='review-content'><p>luptatibus, reiciendis. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quia, suscipit?</p></div>
+
+          </div>
+          <div className="reviews">
+            <p>Review by</p>
+            <h1>Gopal kushwah</h1>
+            <p>⭐⭐⭐⭐⭐</p>
+            <hr />
+            <div className='review-content'><p>luptatibus, reiciendis. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quia, suscipit?</p></div>
+
+          </div>
+          <div className="reviews">
+            <p>Review by</p>
+            <h1>Gopal kushwah</h1>
+            <p>⭐⭐⭐⭐⭐</p>
+            <hr />
+            <div className='review-content'><p>luptatibus, reiciendis. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quia, suscipit?</p></div>
+
+          </div>
+          <div className="reviews">
+            <p>Review by</p>
+            <h1>Gopal kushwah</h1>
+            <p>⭐⭐⭐⭐⭐</p>
+            <hr />
+            <div className='review-content'><p>luptatibus, reiciendis. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quia, suscipit?</p></div>
+
+          </div>
+          <div className="reviews">
+            <p>Review by</p>
+            <h1>Gopal kushwah</h1>
+            <p>⭐⭐⭐⭐⭐</p>
+            <hr />
+            <div className='review-content'><p>luptatibus, reiciendis. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quia, suscipit?</p></div>
+
+          </div>
+        </div>
+      </div>
     </>
   )
 }
