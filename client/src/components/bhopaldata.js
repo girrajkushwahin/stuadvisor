e2bd9539a7da499d9a463e831c35c09a9d724043
rootkaalsec <tgirrajindia@gmail.com>
2023-03-14 23:29:43 +0530

    will start for cards for college showing after search, i have sent little data for making card format

---
+++ client/src/components/bhopaldata.js
@@ -0,0 +1,125 @@
+const data = [
+    {
+        "title": "School of Planning and Architecture - [SPA], Bhopal",
+        "logo": "https://images.collegedunia.com/public/college_data/images/logos/14910392631426324093jp.jpg?h=43&w=43&mode=stretch",
+        "location": "Bhopal, Madhya Pradesh",
+        "url": "https://collegedunia.com/college/5673-school-of-planning-and-architecture-spa-bhopal",
+        "reviews": "https://collegedunia.com/college/5673-school-of-planning-and-architecture-spa-bhopal/reviews",
+        "rating": "7.5 / 10",
+        "fee": "₹ 100,000",
+        "course": "M.Planning",
+        "state": "Madhya Pradesh"
+    },
+    {
+        "title": "National Law Institute University - [NLIU], Bhopal",
+        "logo": "https://images.collegedunia.com/public/college_data/images/logos/1489648066Capture.jpg?h=43&w=43&mode=stretch",
+        "location": "Bhopal, Madhya Pradesh",
+        "url": "https://collegedunia.com/university/25686-national-law-institute-university-nliu-bhopal",
+        "reviews": "https://collegedunia.com/university/25686-national-law-institute-university-nliu-bhopal/reviews",
+        "rating": "8.4 / 10",
+        "fee": "₹ 149,500",
+        "course": "LLM- Total Fees",
+        "state": "Madhya Pradesh"
+    },
+    {
+        "title": "Kamla Nehru Mahavidyalaya, Bhopal",
+        "logo": "https://images.collegedunia.com/public/college_data/images/logos/14912200751423475465knmlogo1.jpg?h=80&w=80&mode=stretch",
+        "location": "Bhopal, Madhya Pradesh",
+        "url": "https://collegedunia.com/college/21021-kamla-nehru-mahavidyalaya-bhopal",
+        "reviews": "https://collegedunia.com/college/21021-kamla-nehru-mahavidyalaya-bhopal/courses-fees",
+        "rating": "7.6 / 10",
+        "fee": "₹ 39,000",
+        "course": "B.Ed- 1st Year Fees",
+        "state": "Madhya Pradesh"
+    },
+    {
+        "title": "Indian Institute of Forest Management - [IIFM], Bhopal",
+        "logo": "https://images.collegedunia.com/public/college_data/images/logos/16186479812565962317530964547229322433258706415070061n.png?h=80&w=80&mode=stretch",
+        "location": "Bhopal, Madhya Pradesh",
+        "url": "https://collegedunia.com/college/17873-indian-institute-of-forest-management-iifm-bhopal",
+        "reviews": "https://collegedunia.com/college/17873-indian-institute-of-forest-management-iifm-bhopal/reviews",
+        "rating": "8.8 / 10",
+        "fee": "₹ 780,000",
+        "course": "PG Diploma in Science- Total Fees",
+        "state": "Madhya Pradesh"
+    },
+    {
+        "title": "People's Institute of Management & Research - [PIMR], Bhopal",
+        "logo": "https://images.collegedunia.com/public/college_data/images/logos/1490341223147547761714753197791475218492vsdf.jpg?h=43&w=43&mode=stretch",
+        "location": "Bhopal, Madhya Pradesh",
+        "url": "https://collegedunia.com/college/24361-peoples-institute-of-management-and-research-pimr-bhopal",
+        "reviews": "https://collegedunia.com/college/24361-peoples-institute-of-management-and-research-pimr-bhopal",
+        "rating": "7.0 / 10",
+        "fee": "₹ 30,000",
+        "course": "BBA/BBM - First Year Fees",
+        "state": "Madhya Pradesh"
+    },
+    {
+        "title": "Institute for Excellence in Higher Education - [IEHE], Bhopal",
+        "logo": "https://images.collegedunia.com/public/college_data/images/logos/1424673709Capture.jpg?h=80&w=80&mode=stretch",
+        "location": "Bhopal, Madhya Pradesh",
+        "url": "https://collegedunia.com/college/2004-institute-for-excellence-in-higher-education-iehe-bhopal",
+        "reviews": "https://collegedunia.com/college/2004-institute-for-excellence-in-higher-education-iehe-bhopal/reviews",
+        "rating": "7.3 / 10 ",
+        "fee": "₹ 29,357 (Total Fees)",
+        "course": "M.Phil",
+        "state": "Madhya Pradesh"
+    },
+    {
+        "title": "Government M L B Girls PG College - [MLB], Bhopal",
+        "logo": "https://images.collegedunia.com/public/college_data/images/logos/1491201323logo..jpg?h=80&w=80&mode=stretch",
+        "location": "Bhopal, Madhya Pradesh",
+        "url": "https://collegedunia.com/college/1628-government-m-l-b-girls-pg-college-mlb-bhopal",
+        "reviews": "https://collegedunia.com/college/1628-government-m-l-b-girls-pg-college-mlb-bhopal/reviews",
+        "rating": "6.9 / 10",
+        "fee": "₹ 14,000 - First Year Fees",
+        "course": "M.Sc",
+        "state": "Madhya Pradesh"
+    },
+    {
+        "title": "Career College of Law, Bhopal",
+        "logo": "https://images.collegedunia.com/public/college_data/images/logos/1491037404logo.png?h=80&w=80&mode=stretch",
+        "location": "Bhopal, Madhya Pradesh",
+        "url": "https://collegedunia.com/college/8658-career-college-of-law-bhopal",
+        "reviews": "https://collegedunia.com/college/8658-career-college-of-law-bhopal/reviews",
+        "rating": "7.3 / 10",
+        "fee": "₹ 33,000",
+        "course": "LLB- 1st Year Fees",
+        "state": "Madhya Pradesh"
+    },
+    {
+        "title": "Barkatullah University - [BU], Bhopal",
+        "logo": "https://images.collegedunia.com/public/college_data/images/logos/1434430370banasr.jpg?h=80&w=80&mode=stretch",
+        "location": "Bhopal, Madhya Pradesh",
+        "url": "https://collegedunia.com/university/25660-barkatullah-university-bu-bhopal",
+        "reviews": "https://collegedunia.com/university/25660-barkatullah-university-bu-bhopal/reviews",
+        "rating": "7.2 / 10",
+        "fee": "₹ 9,795 - Total Fees",
+        "course": "MA",
+        "state": "Madhya Pradesh"
+    },
+    {
+        "title": "VIT Bhopal University, Bhopal",
+        "logo": "https://images.collegedunia.com/public/college_data/images/logos/1619152120Untitled.png?h=80&w=80&mode=stretch",
+        "location": "Bhopal, Madhya Pradesh",
+        "url": "https://collegedunia.com/university/59253-vit-bhopal-university-bhopal",
+        "reviews": "https://collegedunia.com/university/59253-vit-bhopal-university-bhopal/reviews",
+        "rating": "8 / 10",
+        "fee": "₹ 201,000",
+        "course": "BE/B.Tech - 1st Year Fees",
+        "state": "Madhya Pradesh"
+    },
+    {
+        "title": "Trinity Institute of Technology and Research, Bhopal",
+        "logo": "https://images.collegedunia.com/public/college_data/images/logos/1649677325TrinityLogoHorizontal.png?h=80&w=80&mode=stretch",
+        "location": "Bhopal, Madhya Pradesh",
+        "url": "https://collegedunia.com/college/15492-trinity-institute-of-technology-and-research-bhopal",
+        "reviews": "https://collegedunia.com/college/15492-trinity-institute-of-technology-and-research-bhopal/reviews",
+        "rating": "8.1 / 10",
+        "fee": "₹ 53,000 ",
+        "course": "BE/B.Tech- 1st Year Fees",
+        "state": "Madhya Pradesh"
+    }
+]
+
+export default data;
