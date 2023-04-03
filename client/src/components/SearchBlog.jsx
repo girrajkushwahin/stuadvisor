d6bfa34bdb9219d4743c72e874d350c160012485
gopalkushwah <gopalkushwah15101999@gmail.com>
2023-04-03 22:26:17 +0530

    blogs frontend done

--- client/src/components/SearchBlog.jsx
+++ client/src/components/SearchBlog.jsx
@@ -5,10 +5,14 @@ import boyProfile from '../images/boy-profile.png'
 
 const SearchBlog = () => {
   const [data, setData] = useState('');
+  const [hide, setHide] = useState(true);
   // const instantData = [item1, item2, item3, item4, item5];
 
   const handleSearch = data => { }
 
+  const hideCardContent = () => {
+    setHide(!hide);
+  }
   return (
     <>
       <div className="search-blog-container">
@@ -22,12 +26,13 @@ const SearchBlog = () => {
           </div>
         </div>
         <div className="blog-cards-container">
-          <div className="blog-card">
+          {/* ------------ */}
+          <div className="blog-card" onClick={hideCardContent}>
             <div className="blog-user-profile">
               <img src={boyProfile} alt="boy profile" />
             </div>
             <div className="blog-head-content">
-              <h1>What is react ? useState hook in react .</h1>
+              <h1>What is IoT? What technologies have made IoT possible?</h1>
               <span>posted by : <span className="blog-username">Gopal kushwah</span></span>
               <span>posted on : <span className="blog-date">01/01/2023</span> </span>
             </div>
@@ -35,25 +40,33 @@ const SearchBlog = () => {
               <span>+</span>
             </div>
           </div>
-          <div className="blog-card">
-            <div className="blog-user-profile">
-              <img src={girlProfile} alt="boy profile" />
+          <div className={hide ? "blog-card-content-container blog-card-content-container-hide" : "blog-card-content-container"}>
+            <div className="blog-main-content">
+              <span><h2 className='main-heading'>What is IoT? What technologies have made IoT possible?</h2></span>
+              <hr />
+              <h3>What is IoT?</h3>
+              <p>The Internet of Things (IoT) describes the network of physical objects—“things”—that are embedded with sensors, software, and other technologies for the purpose of connecting and exchanging data with other devices and systems over the internet. These devices range from ordinary household objects to sophisticated industrial tools. With more than 7 billion connected IoT devices today, experts are expecting this number to grow to 10 billion by 2020 and 22 billion by 2025. Oracle has a network of device partners.</p>
+              <h3>Why is Internet of Things (IoT) so important?</h3>
+              <p>The Internet of Things (IoT) describes the network of physical objects—“things”—that are embedded with sensors, software, and other technologies for the purpose of connecting and exchanging data with other devices and systems over the internet. These devices range from ordinary household objects to sophisticated industrial tools. With more than 7 billion connected IoT devices today, experts are expecting this number to grow to 10 billion by 2020 and 22 billion by 2025. Oracle has a network of device partners.</p>
             </div>
-            <div className="blog-head-content">
-              <h1>What is react ? useState hook in react .</h1>
-              <span>posted by : <span className="blog-username">Gopal kushwah</span></span>
-              <span>posted on : <span className="blog-date">01/01/2023</span> </span>
-            </div>
-            <div className="blog-accordian-icon">
-              <span>+</span>
+            <div className="blog-content-bottom">
+              <div className="blog-user-profile">
+                <img src={boyProfile} alt="boy profile" />
+                <p>Author : Gopal kushwah</p>
+                <span className="close-ic on"> ^ </span>
+              </div>
+
             </div>
           </div>
-          <div className="blog-card">
+
+
+          {/* --------------------- */}
+          <div className="blog-card" onClick={hideCardContent}>
             <div className="blog-user-profile">
               <img src={boyProfile} alt="boy profile" />
             </div>
             <div className="blog-head-content">
-              <h1>What is react ? useState hook in react .What is react ? useState hook in react .What is react ? useState hook in react .What is react ? useState hook in react .</h1>
+              <h1>What is IoT? What technologies have made IoT possible?</h1>
               <span>posted by : <span className="blog-username">Gopal kushwah</span></span>
               <span>posted on : <span className="blog-date">01/01/2023</span> </span>
             </div>
@@ -61,47 +74,28 @@ const SearchBlog = () => {
               <span>+</span>
             </div>
           </div>
-          <div className="blog-card">
-            <div className="blog-user-profile">
-              <img src={girlProfile} alt="boy profile" />
+          <div className={hide ? "blog-card-content-container blog-card-content-container-hide" : "blog-card-content-container"}>
+            <div className="blog-main-content">
+              <span><h2 className='main-heading'>What is IoT? What technologies have made IoT possible?</h2></span>
+              <hr />
+              <h3>What is IoT?</h3>
+              <p>The Internet of Things (IoT) describes the network of physical objects—“things”—that are embedded with sensors, software, and other technologies for the purpose of connecting and exchanging data with other devices and systems over the internet. These devices range from ordinary household objects to sophisticated industrial tools. With more than 7 billion connected IoT devices today, experts are expecting this number to grow to 10 billion by 2020 and 22 billion by 2025. Oracle has a network of device partners.</p>
+              <h3>Why is Internet of Things (IoT) so important?</h3>
+              <p>The Internet of Things (IoT) describes the network of physical objects—“things”—that are embedded with sensors, software, and other technologies for the purpose of connecting and exchanging data with other devices and systems over the internet. These devices range from ordinary household objects to sophisticated industrial tools. With more than 7 billion connected IoT devices today, experts are expecting this number to grow to 10 billion by 2020 and 22 billion by 2025. Oracle has a network of device partners.</p>
             </div>
-            <div className="blog-head-content">
-              <h1>What is react ? useState hook in react .</h1>
-              <span>posted by : <span className="blog-username">Gopal kushwah</span></span>
-              <span>posted on : <span className="blog-date">01/01/2023</span> </span>
-            </div>
-            <div className="blog-accordian-icon">
-              <span>+</span>
-            </div>
-          </div>
-          <div className="blog-card">
-            <div className="blog-user-profile">
-              <img src={girlProfile} alt="boy profile" />
-            </div>
-            <div className="blog-head-content">
-              <h1>What is react ? useState hook in react .</h1>
-              <span>posted by : <span className="blog-username">Gopal kushwah</span></span>
-              <span>posted on : <span className="blog-date">01/01/2023</span> </span>
-            </div>
-            <div className="blog-accordian-icon">
-              <span>+</span>
+            <div className="blog-content-bottom">
+              <div className="blog-user-profile">
+                <img src={boyProfile} alt="boy profile" />
+                <p>Author : Gopal kushwah</p>
+                <span className="close-ic on"> ^ </span>
+              </div>
+
             </div>
           </div>
-          <div className="blog-card">
-            <div className="blog-user-profile">
-              <img src={girlProfile} alt="boy profile" />
-            </div>
-            <div className="blog-head-content">
-              <h1>What is react ? useState hook in react .</h1>
-              <span>posted by : <span className="blog-username">Gopal kushwah</span></span>
-              <span>posted on : <span className="blog-date">01/01/2023</span> </span>
-            </div>
-            <div className="blog-accordian-icon">
-              <span>+</span>
-            </div>
-          </div>
+
+          {/* ----------------------- */}
         </div>
-      </div>
+      </div >
     </>
   )
 }
--- client/src/components/Trendingblogs.jsx
+++ client/src/components/Trendingblogs.jsx
@@ -1,18 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
+
 import girlProfile from '../images/girl-profile.png'
 import boyProfile from '../images/boy-profile.png'
 
 const TrendingBlogs = () => {
+  const [hide, setHide] = useState(true);
+
+  const hideCardContent = () => {
+    setHide(!hide);
+  }
   return (
     <>
       <div className="trending-blog-container">
         <div className="blog-cards-container">
-          <div className="blog-card">
+          <div className="blog-card" onClick={hideCardContent}>
             <div className="blog-user-profile">
               <img src={boyProfile} alt="boy profile" />
             </div>
             <div className="blog-head-content">
-              <h1>What is react ? useState hook in react .</h1>
+              <h1>What is IoT? What technologies have made IoT possible?</h1>
               <span>posted by : <span className="blog-username">Gopal kushwah</span></span>
               <span>posted on : <span className="blog-date">01/01/2023</span> </span>
             </div>
@@ -20,32 +26,25 @@ const TrendingBlogs = () => {
               <span>+</span>
             </div>
           </div>
-          <div className="blog-card">
-            <div className="blog-user-profile">
-              <img src={girlProfile} alt="boy profile" />
+          <div className={hide ? "blog-card-content-container blog-card-content-container-hide" : "blog-card-content-container"}>
+            <div className="blog-main-content">
+              <span><h2 className='main-heading'>What is IoT? What technologies have made IoT possible?</h2></span>
+              <hr />
+              <h3>What is IoT?</h3>
+              <p>The Internet of Things (IoT) describes the network of physical objects—“things”—that are embedded with sensors, software, and other technologies for the purpose of connecting and exchanging data with other devices and systems over the internet. These devices range from ordinary household objects to sophisticated industrial tools. With more than 7 billion connected IoT devices today, experts are expecting this number to grow to 10 billion by 2020 and 22 billion by 2025. Oracle has a network of device partners.</p>
+              <h3>Why is Internet of Things (IoT) so important?</h3>
+              <p>The Internet of Things (IoT) describes the network of physical objects—“things”—that are embedded with sensors, software, and other technologies for the purpose of connecting and exchanging data with other devices and systems over the internet. These devices range from ordinary household objects to sophisticated industrial tools. With more than 7 billion connected IoT devices today, experts are expecting this number to grow to 10 billion by 2020 and 22 billion by 2025. Oracle has a network of device partners.</p>
             </div>
-            <div className="blog-head-content">
-              <h1>What is react ? useState hook in react .</h1>
-              <span>posted by : <span className="blog-username">Gopal kushwah</span></span>
-              <span>posted on : <span className="blog-date">01/01/2023</span> </span>
-            </div>
-            <div className="blog-accordian-icon">
-              <span>+</span>
-            </div>
-          </div>
-          <div className="blog-card">
-            <div className="blog-user-profile">
-              <img src={boyProfile} alt="boy profile" />
-            </div>
-            <div className="blog-head-content">
-              <h1>What is react ? useState hook in react .What is react ? useState hook in react .What is react ? useState hook in react .What is react ? useState hook in react .</h1>
-              <span>posted by : <span className="blog-username">Gopal kushwah</span></span>
-              <span>posted on : <span className="blog-date">01/01/2023</span> </span>
-            </div>
-            <div className="blog-accordian-icon">
-              <span>+</span>
+            <div className="blog-content-bottom">
+              <div className="blog-user-profile">
+                <img src={boyProfile} alt="boy profile" />
+                <p>Author : Gopal kushwah</p>
+                <span className="close-ic on"> ^ </span>
+              </div>
+
             </div>
           </div>
+
         </div>
       </div>
     </>
